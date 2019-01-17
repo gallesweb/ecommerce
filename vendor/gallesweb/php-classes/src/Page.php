1a71afd01f9bc445cc07f9ab3d27ba69d8f55ec2
gallesweb <dangalles@gmail.com>
2019-01-17 18:49:33 -0200

    criando o pageadmin e tpladmin

--- vendor/gallesweb/php-classes/src/Page.php
+++ vendor/gallesweb/php-classes/src/Page.php
@@ -12,12 +12,12 @@ class Page{
 		"data"=>[]
 	];
 
-	public function __construct($opts = array()){
+	public function __construct($opts = array(), $tpl_dir = "/views/"){
 
 		$this->options = array_merge($this->defaults, $opts);
 
 		$config = array(
-						"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"]."/views/", //a patir do root procure a pasta tal
+						"tpl_dir"       => $_SERVER["DOCUMENT_ROOT"].$tpl_dir, //a patir do root procure a pasta tal
 						"cache_dir"     => $_SERVER["DOCUMENT_ROOT"]."/views-cache/",
 						"debug"         => false // set to false to improve the speed
 					   );
