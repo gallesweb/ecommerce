e2a2007af892c17f95fdd2103b31e58d7180068a
gallesweb <dangalles@gmail.com>
2019-01-19 18:10:05 -0200

    Site - Produtos

--- site.php
+++ site.php
@@ -1,12 +1,17 @@
 <?php
 
 use \gallesweb\Page;
+use \gallesweb\Model\Product;
 
 $app->get('/', function() {
+
+	$products = Product::listAll();
     
 	$page = new Page();
 
-	$page->setTpl("index");
+	$page->setTpl("index", [
+		"products"=>Product::checkList($products)
+	]);
 
 });
 
