3033a4997e54a42dfd3de911a23907d5524b7387
gallesweb <dangalles@gmail.com>
2019-01-21 16:30:09 -0200

    Site - Esqueceu a Senha

--- vendor/gallesweb/php-classes/src/Model/User.php
+++ vendor/gallesweb/php-classes/src/Model/User.php
@@ -253,18 +253,28 @@ class User extends Model{
 				return $data;*/
 
 				$dataRecovery = $results2[0];
+
 	             $iv = random_bytes(openssl_cipher_iv_length('aes-256-cbc'));
+
 	             $code = openssl_encrypt($dataRecovery['idrecovery'], 'aes-256-cbc', User::SECRET, 0, $iv);
+
 	             $result = base64_encode($iv.$code);
+
 	             if ($inadmin === true) {
+
 	                 $link = "http://www.galleswebecommerce.com.br/admin/forgot/reset?code=$result";
+
 	             } else {
+
 	                 $link = "http://www.galleswebecommerce.com.br/forgot/reset?code=$result";
+
 	             } 
+
 	             $mailer = new Mailer($data['desemail'], $data['desperson'], "Redefinir senha da GallesWeb Store", "forgot", array(
 	                 "name"=>$data['desperson'],
 	                 "link"=>$link
 	             )); 
+	             
 	             $mailer->send();
 	             return $link;
 
